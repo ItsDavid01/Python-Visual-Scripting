da82e4758ff8ffcb4ae5345d43befd3654be70fd
Juan Maestre <98894997+JuandiGo1@users.noreply.github.com>
2024-05-13 18:56:01 -0500

    Add files via upload

--- blocks.py
+++ blocks.py
@@ -1,3 +1,11 @@
+variable_dict = {}
+
+class bloque():
+    def __init__(self):
+        self.i_flow = False
+        self.o_flow = self.i_flow
+
+'''Bloques de flujo'''
 class On_Start():
     def __init__(self):
         self.i_flow = True
@@ -12,16 +20,55 @@ class cycle():
         self.i_flow = True
         self.o_flow = self.i_flow
 
-class float_literal():
-    def __init__(self):
-        self.o_value = 0.0
+'''Bloques de Asigancion''' 
+
+class set_var(bloque):
+    def execute(self):
+        while self.i_flow == False:
+            pass
         
-class string_literal():
-    def __init__(self):
-        self.o_value = ""
+        variable_dict[self.i_name] = self.i_value
+        self.o_variable_created = self.i_value
         
-class print():
-    
+    def __init__(self):
+
+        self.i_name = ""
+        self.i_value = None
+        self.o_variable_created = None
+
+class set_arr(bloque):
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        
+        variable_dict[self.i_name] = [x for x in self.i_value if x!= ',']
+        self.o_variable_created = self.i_value
+        
+    def __init__(self):
+        self.i_name = ""
+        self.i_value = ""
+        self.o_variable_created = None
+
+class call_var(bloque):
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        
+        self.o_value = variable_dict.get(self.i_name, None)
+
+        
+    def __init__(self):
+        self.i_name = ""
+        self.o_value = None
+
+'''Bloques de Ejecución''' 
+
+class log(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        print(self.i_value)
+
     def __init__(self):
         self.i_value = None
         
@@ -42,8 +89,118 @@ class branch():
         self.o_false_flow = False
         self.i_condition = None
         
+class compare(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        if self.i_A_value < self.i_B_value :
+            self.o_menorque = True
+        if self.i_A_value > self.i_B_value :
+            self.o_mayorque = True
+        if self.i_A_value == self.i_B_value :
+            self.o_igual = True
+        if self.i_A_value != self.i_B_value :
+            self.o_desigual = True
+
+    def __init__(self):
         
+        self.i_A_value = None
+        self.i_B_value = None
+        self.o_menorque = False
+        self.o_mayorque = False
+        self.o_igual = False
+        self.o_desigual = False
+                  
+      
+class for_iter(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        print(self.i_value)
+
+    def __init__(self):
+        self.i_flow = False
+        self.i_value = None     
     
+'''Bloques operacionales'''
+
+class add(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        self.o_result = self.i_A_value + self.i_B_value
+
+    def __init__(self):
+        self.i_A_value = 0 
+        self.i_B_value = 0 
+        self.o_result= 0
+
+class sub(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        self.o_result = self.i_A_value - self.i_B_value
+
+    def __init__(self):
+        self.i_A_value = 0 
+        self.i_B_value = 0 
+        self.o_result= 0
+
+
+class mult(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        self.o_result = self.i_A_value * self.i_B_value
+
+    def __init__(self):
+        self.i_A_value = 0 
+        self.i_B_value = 0 
+        self.o_result= 0 
+
+class div(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        self.o_result = self.i_A_value / self.i_B_value
+
+    def __init__(self):
+        self.i_A_value = 0 
+        self.i_B_value = 0 
+        self.o_result= 0 
+
+class div_int(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        self.o_result = self.i_A_value // self.i_B_value
+
+    def __init__(self):
+        self.i_A_value = 0 
+        self.i_B_value = 0 
+        self.o_result= 0 
+
+class mod(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
+        self.o_result = self.i_A_value % self.i_B_value
+
+    def __init__(self):
+        self.i_A_value = 0 
+        self.i_B_value = 0 
+        self.o_result= 0 
+
+
+class append_arr(bloque):   
+    def execute(self):
+        while self.i_flow == False:
+            pass
         
+        self.o_value = variable_dict.get(self.i_name, None)
+
         
-    
+    def __init__(self):
+        self.i_flow = False
+        self.i_name = ""
+        self.o_value = None 
